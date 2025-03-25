d189226fec716c4d36353dc58888da683afbe5f3
Casey Driver <Casey.R.Driver@gmail.com>
2025-03-24 19:09:17 -0700

    Replaced images with links

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,28 @@
 const initialCards = [
-    {name: "la", link: "images/la.jpg"},
-    {name: "sd", link: "images/sd.jpg"},
-    {name: "seattle", link: "images/seattle.jpg"},
-    {name: "nashville", link: "images/nashville.jpg"},
-    {name: "sedona", link: "images/sedona.jpg"},
-    {name: "honolulu", link: "images/honolulu.jpg"},
-]
+  {
+    name: "la",
+    link: "https://unsplash.com/photos/a-view-of-downtown-los-angeles-california-with-palm-trees-in-the-foreground-Zh0s1o3Zi5Y",
+  },
+  {
+    name: "sd",
+    link: "https://unsplash.com/photos/a-high-angle-of-port-of-san-diego-on-a-sunny-day-clear-sky-background-IujeKFs_FFU",
+  },
+  {
+    name: "seattle",
+    link: "https://unsplash.com/photos/a-view-of-a-city-with-a-tower-in-the-middle-of-it-JEicDFy5Cd8",
+  },
+  {
+    name: "nashville",
+    link: "https://unsplash.com/photos/a-view-of-a-city-with-tall-buildings-8GOtEYr7qho",
+  },
+  {
+    name: "sedona",
+    link: "https://unsplash.com/photos/brown-rocky-mountain-under-blue-sky-during-daytime-tfqrqGH7S9A",
+  },
+  {
+    name: "honolulu",
+    link: "https://unsplash.com/photos/buildings-near-body-of-water-XwCgFPLalP4",
+  },
+];
 
 console.log(initialCards);
