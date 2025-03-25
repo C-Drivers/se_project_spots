8cbcf7bacd8767b5b7fd09f90e42f31db031c0bb
Casey Driver <Casey.R.Driver@gmail.com>
2025-03-24 18:56:36 -0700

    Project 4 Stage 1

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const initialCards = [
+    {name: "la", link: "images/la.jpg"},
+    {name: "sd", link: "images/sd.jpg"},
+    {name: "seattle", link: "images/seattle.jpg"},
+    {name: "nashville", link: "images/nashville.jpg"},
+    {name: "sedona", link: "images/sedona.jpg"},
+    {name: "honolulu", link: "images/honolulu.jpg"},
+]
+
+console.log(initialCards);
