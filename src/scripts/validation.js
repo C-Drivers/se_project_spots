efd04fbead04e17a20894385fa9ff89a8e4afd30
Casey Driver <Casey.R.Driver@gmail.com>
2026-02-11 11:55:30 -0800

    Webpack integration.

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,5 +1,5 @@
 //Validation Stage 9
-const settings = {
+export const settings = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__save-btn",
@@ -8,19 +8,19 @@ const settings = {
   errorClass: "modal__error",
 };
 
-const showInputError = (formEl, inputEl, settings) => {
+export const showInputError = (formEl, inputEl, settings) => {
   const errorEl = formEl.querySelector(`#${inputEl.id}-error`);
   inputEl.classList.add(settings.inputErrorClass);
   errorEl.textContent = inputEl.validationMessage;
 };
 
-const hideInputError = (formEl, inputEl, settings) => {
+export const hideInputError = (formEl, inputEl, settings) => {
   const errorEl = formEl.querySelector(`#${inputEl.id}-error`);
   inputEl.classList.remove(settings.inputErrorClass);
   errorEl.textContent = "";
 };
 
-const checkInputValidity = (formEl, inputEl, settings) => {
+export const checkInputValidity = (formEl, inputEl, settings) => {
   if (!inputEl.validity.valid) {
     showInputError(formEl, inputEl, settings);
   } else {
@@ -28,13 +28,13 @@ const checkInputValidity = (formEl, inputEl, settings) => {
   }
 };
 
-const hasInvalidInput = (inputList) => {
+export const hasInvalidInput = (inputList) => {
   return inputList.some((inputEl) => {
     return !inputEl.validity.valid;
   });
 };
 
-const toggleButtonState = (inputList, buttonEl, settings) => {
+export const toggleButtonState = (inputList, buttonEl, settings) => {
   if (hasInvalidInput(inputList)) {
     disableButton(buttonEl);
     buttonEl.classList.add(settings.inactiveButtonClass);
@@ -44,11 +44,11 @@ const toggleButtonState = (inputList, buttonEl, settings) => {
   }
 };
 
-const disableButton = (buttonEl) => {
+export const disableButton = (buttonEl) => {
   buttonEl.disabled = true;
 };
 
-const resetValidation = (formEl, inputList, settings) => {
+export const resetValidation = (formEl, inputList, settings) => {
   inputList.forEach((input) => {
     hideInputError(formEl, input, settings);
   });
@@ -58,7 +58,7 @@ const resetValidation = (formEl, inputList, settings) => {
   buttonEl.classList.add(settings.inactiveButtonClass);
 };
 
-const setEventListeners = (formEl, settings) => {
+export const setEventListeners = (formEl, settings) => {
   const inputList = Array.from(formEl.querySelectorAll(settings.inputSelector));
   const buttonEl = formEl.querySelector(settings.submitButtonSelector);
 
@@ -72,7 +72,7 @@ const setEventListeners = (formEl, settings) => {
   });
 };
 
-const enableValidation = (settings) => {
+export const enableValidation = (settings) => {
   const formList = document.querySelectorAll(settings.formSelector);
   formList.forEach((formEl) => {
     setEventListeners(formEl, settings);
